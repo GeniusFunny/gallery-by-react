b6bd07456ee9fac04bc647eb28e17ddb105c72de
GeniusFunny <865753437@qq.com>
2018-02-10 21:35:03 +0800

    init

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,52 +1,17 @@
-require('normalize.css');
-require('../styles/App.scss');
+require('normalize.css/normalize.css');
+require('styles/App.css');
+
 import React from 'react';
 
-//获取图片相关数据
-let imageDatas = require('../data/imageData.json');
-//利用自执行函数， 将图片名信息转成图片URL路径信息
-imageDatas = (function genImangeURL(ImageDataArr) {
-  for (let i = 0, j = ImageDataArr.length; i < j; i++) {
-    let singleImageData = ImageDataArr[i];
-
-    singleImageData.imageURL = require('../images/' + singleImageData.fileName);
-    ImageDataArr[i] = singleImageData;
-  }
-  return ImageDataArr;
-})(imageDatas);
-
-class ImageFigure extends React.Component {
-  render() {
-    return (
-      <figure className="img-figure">
-        <img src={this.props.data.imageURL} alt={this.props.data.title}
-        />
-        <figcaption>
-          <h2 className="img-title">{this.props.data.title}</h2>
-        </figcaption>
-      </figure>
-    )
-  }
-}
+let yeomanImage = require('../images/yeoman.png');
 
 class AppComponent extends React.Component {
   render() {
-
-    let controllerUnits = [],
-        imgFigures = [];
-      imageDatas.forEach(function(value) {
-        imgFigures.push(<ImageFigure data={value} />);
-      });
     return (
-      <section className="stage">
-        <section className='img-sec'>
-          {imgFigures}
-        </section>
-        <nav className="controller-nav">
-          {controllerUnits}
-        </nav>
-
-      </section>
+      <div className="index">
+        <img src={yeomanImage} alt="Yeoman Generator" />
+        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+      </div>
     );
   }
 }
