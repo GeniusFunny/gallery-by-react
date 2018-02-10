95fac89976127119217e30f38ba5f25a6e9e0037
GeniusFunny <865753437@qq.com>
2018-02-11 00:02:43 +0800

    nav

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/Main';
+//import App from './components/Main';
+import Nav from './components/navbar';
 
 // Render the main component into the dom
-ReactDOM.render(<App />, document.getElementById('app'));
+const navItems = {
+  datas: ['首页', '关于', '加入我们'],
+  className: 'normal'
+};
+ReactDOM.render(
+  <Nav>{navItems}</Nav>,
+  document.getElementById('app')
+);
